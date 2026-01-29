b4a04f3da9473030c54d250f168a60fb1567d6a8
Vaishnavi Mali <vaishumali85@gmail.com>
2026-01-29 12:23:36 +0530

    Pass function as props

--- Simple_cards/src/components/Card.jsx
+++ Simple_cards/src/components/Card.jsx
@@ -1,16 +1,30 @@
-const Card = (props) => {
-    return (
-        <>
-            <div>
-                <div className='card'>
-                    <img src={props.img} alt='Image' />
-                    <h1>Hello, I'm {props.user}</h1>
-                    <h3> I'm {props.age} years old</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, nihil.</p>
-                </div>
-            </div>
-        </>
-    )
-}
+import { useState } from "react";
 
-export default Card
+const Card = (props) => {
+  const [likes, setLikes] = useState(0);
+  return (
+    <>
+      <div>
+        <div className="card">
+          <img src={props.img} alt="Image" />
+          <h1>Hello, I'm {props.user}</h1>
+          <h3>
+            {" "}
+            I'm {props.age} years old
+            <button onClick={() => props.greetingUser(props.user)}>
+              Greet
+            </button>
+            <button onClick={() => setLikes(likes + 1)}>❤️ {likes}</button>
+          </h3>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente,
+            nihil.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
